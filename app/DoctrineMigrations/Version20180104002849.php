566374311d98c7685ed4cfa01784b8973da18e1a
gavin <gavin_erickson@hotmail.com>
2018-01-04 00:32:45 +0000

    doctrine migration change linemanger to a manytoone self referencing to avoid "unique" issues on previous version

---
+++ app/DoctrineMigrations/Version20180104002849.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180104002849 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user DROP INDEX UNIQ_8D93D64985A16E0C, ADD INDEX IDX_8D93D64985A16E0C (linemanager_id)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user DROP INDEX IDX_8D93D64985A16E0C, ADD UNIQUE INDEX UNIQ_8D93D64985A16E0C (linemanager_id)');
+    }
+}
